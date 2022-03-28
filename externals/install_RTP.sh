f647961047fe4ea048274a57120642a558983a66
kazuki0824 <kazukuro0824@gmail.com>
2022-03-29 03:41:16 +0900

    Fix ENV

--- externals/install_RTP.sh
+++ externals/install_RTP.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -eu
+PKGS='openjdk-8-jdk ant'
+sudo apt install -y --no-install-recommends $PKGS
 
 tar zxvf eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
 
 cd OpenRTP-aist
-ECLIPSE_HOME=$PWD/../eclipse
+export ECLIPSE_HOME=$PWD/../eclipse
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 ./build_all
 
 sed -i -e "s#file:///home/openrtm/public_html/pub/eclipse/projects/oxygen,##" ./install_plugins
 ./install_plugins $ECLIPSE_HOME
 
 unzip -j -d ../eclipse/dropins openrtp-1.2.2.*.zip
+
+#sudo apt purge $PKGS --auto-remove
