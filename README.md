0517bbcc79a9f5be43e2f1b3951ac5851337305b
kazuki0824 <kazukuro0824@gmail.com>
2022-03-29 12:21:38 +0900

    Update README Add executable attrs to scripts

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 ## How to use
 
 1. Clone this recursively ```git clone https://github.com/kazuki0824/hlab-nxo-setup.git --depth 1 --recursive```
-2. Run ```bash ./externals/install_RTP.sh```
+2. Get some large contents by running ```cd hlab-nxo-setup && git lfs pull```
+2. Enter externals/, and then run ```./install_RTP.sh```
 3. Install ROS1 by following [this](http://wiki.ros.org/melodic/Installation/Ubuntu) instruction 
-4. ```bash ./overlay_ws/setup_rtmros_ws.sh```
+4. Enter overlay\_ws/, and run ```./setup_rtmros_ws.sh```
 5. ```bash ./opennextage_kazuki.sh```
 
 ## How to use (Docker)
--- externals/install_RTP.sh
+++ externals/install_RTP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
+tar zxvf eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
+
 PKGS='openjdk-8-jdk ant'
 sudo apt install -y --no-install-recommends $PKGS
-
-tar zxvf eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
 
 cd OpenRTP-aist
 export ECLIPSE_HOME=$PWD/../eclipse
@@ -13,6 +13,9 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 sed -i -e "s#file:///home/openrtm/public_html/pub/eclipse/projects/oxygen,##" ./install_plugins
 ./install_plugins $ECLIPSE_HOME
 
-unzip -j -d ../eclipse/dropins openrtp-1.2.2.*.zip
+unzip -j -d ../eclipse/dropins openrtp-*.zip
 
-#sudo apt purge $PKGS --auto-remove
+cd .. && rm -r OpenRTP-aist
+
+sudo apt purge $PKGS --auto-remove
+sudo apt install openjdk-8-jre -y --no-install-recommends
