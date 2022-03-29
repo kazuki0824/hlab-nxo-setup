03ae6d2756708ded44b098663c9e5e27b79fca8e
kazuki0824 <kazukuro0824@gmail.com>
2022-03-29 14:21:24 +0900

    Check if the install is taken place within docker build

--- overlay_ws/setup_rtmros_ws.sh
+++ overlay_ws/setup_rtmros_ws.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+
+#https://stackoverflow.com/questions/69860182/how-to-detect-if-the-current-script-is-running-in-a-docker-build
+isDocker(){
+    local cgroup=/proc/1/cgroup
+    test -f $cgroup && [[ "$(<$cgroup)" = *:cpuset:/docker/* ]]
+}
+
+isDockerBuildkit(){
+    local cgroup=/proc/1/cgroup
+    test -f $cgroup && [[ "$(<$cgroup)" = *:cpuset:/docker/buildkit/* ]]
+}
+
+isDockerContainer(){
+    [ -e /.dockerenv ]
+}
+
+if isDockerBuildkit || (isDocker && ! isDockerContainer) then
+  IN_BUILD=1
+else
+  IN_BUILD=0
+fi
+
+
 set -e
 cd src
 . /opt/ros/${ROS_DISTRO:-melodic}/setup.bash
@@ -10,23 +33,25 @@ git clone https://github.com/start-jsk/rtmros_hironx.git --depth 1 ; \
 
 set -e
 cd ../..
-if [ ! -f /.dockerenv ] && [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+if [ $IN_BUILD -e 0 ] && [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
     sudo rosdep init
 fi
 rosdep update --include-eol-distros && rosdep install -y -i --from-paths src
 catkin config --install
 catkin b
+catkin test
 
 
-if [ -f /.dockerenv ]; then
-    cp -r ./install /opt/preinstalled
+if [ $IN_BUILD -ne 0 ] then
+    cp -v -r ./install /opt/preinstalled
 else
+    echo "Install files to /opt/preinstalled..."
     sudo cp -r ./install /opt/preinstalled
     
     cd ..
-    read -n1 -p "Do you want to add environment setup to ~/.bashrc? (y/N): " yn
+    read -p "Do you want to add environment setup to ~/.bashrc? (y/N): " yn
     if [[ $yn = [yY] ]]; then
-        realpath ./rtm_entrypoint.sh >> ~/.bashrc
+        echo "source /opt/preinstalled/setup.bash" >> ~/.bashrc
     fi
     sudo rm -r overlay_ws/
 fi
