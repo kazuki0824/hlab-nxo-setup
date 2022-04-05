900460f1e8b97c6efd0eab12ae17c9504dcda3be
kazuki0824 <kazukuro0824@gmail.com>
2022-04-05 18:23:07 +0900

    Remove submodules

--- setup_choreonoid.sh
+++ setup_choreonoid.sh
@@ -25,37 +25,45 @@ fi
 git clone https://github.com/choreonoid/choreonoid.git --depth 1 -b $CNOID_TAG
 
 ln -s `realpath ./grasp-plugin` ./choreonoid/ext/graspPlugin
-echo "Ubuntu $VERSION_ID is selected. Installing dependencies..."
-wget https://raw.githubusercontent.com/choreonoid/choreonoid/v1.7.0/misc/script/install-requisites-ubuntu-$VERSION_ID.sh -P ./choreonoid/misc/script/ -O install-requisites-ubuntu-$VERSION_ID.sh || echo 'Fetching has been skipped since not found.'
+echo "Ubuntu $VERSION_ID is selected. Installing dependencies...\n"
 
+
+wget --spider https://raw.githubusercontent.com/choreonoid/choreonoid/v1.7.0/misc/script/install-requisites-ubuntu-$VERSION_ID.sh
+if [ $? -eq 0 ]; then
+  rm ./choreonoid/misc/script/install-requisites-ubuntu-$VERSION_ID.sh
+  wget https://raw.githubusercontent.com/choreonoid/choreonoid/v1.7.0/misc/script/install-requisites-ubuntu-$VERSION_ID.sh -P ./choreonoid/misc/script/
+else
+  echo 'Fetching has been skipped since not found.\n'
+fi
 source ./choreonoid/misc/script/install-requisites-ubuntu-$VERSION_ID.sh
 
 
-echo "Entering build-choreonoid/..."
+echo "Entering build-choreonoid/...\n"
 mkdir ./build-choreonoid && cd ./build-choreonoid
 cmake ../choreonoid -DGRASP_PLUGINS=$GRASP_PLUGINS \
 -DGRASP_ROBOT_MODELS_PLUGINS=$GRASP_ROBOT_MODELS_PLUGINS \
--DBUILD_GRASP_PCL_PLUGIN=ON
+-DBUILD_GRASP_PCL_PLUGIN=ON && \
+-DUSE_QT5=ON
 LIBRARY_PATH=/opt/ros/${ROS_DISTRO}/lib make -j`nproc` -k && cd ..
-echo "Leaving build-choreonoid/..."
+echo "Leaving build-choreonoid/...\n"
 
 
-echo "Regenerate IDL..."
-ARRAY[0]="./hlab-nxo-setup/externals/hironx-interface/HiroNXInterface/HiroNXGUI/HIROController.idl"
-ARRAY[1]="./hlab-nxo-setup/externals/hironx-interface/HiroNXInterface/HiroNXGUI/HiroNX.idl"
-ARRAY[2]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/NextageInterface/HIROController.idl"
-ARRAY[3]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/NextageInterface/HiroNX.idl"
-ARRAY[4]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/PortDuplicator/HIROController.idl"
-ARRAY[5]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/PortDuplicator/HiroNX.idl"
-ARRAY[6]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/THK/HIROController.idl"
-ARRAY[7]="./choreonoid/ext/graspPlugin/RobotInterface/Nextage/THK/HiroNX.idl"
+echo "Regenerate IDL...\n"
+ARRAY[0]="hlab-nxo-setup/externals/hironx-interface/HiroNXInterface/HiroNXGUI/HIROController.idl"
+ARRAY[1]="hlab-nxo-setup/externals/hironx-interface/HiroNXInterface/HiroNXGUI/HiroNX.idl"
+ARRAY[2]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/NextageInterface/HIROController.idl"
+ARRAY[3]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/NextageInterface/HiroNX.idl"
+ARRAY[4]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/PortDuplicator/HIROController.idl"
+ARRAY[5]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/PortDuplicator/HiroNX.idl"
+ARRAY[6]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/THK/HIROController.idl"
+ARRAY[7]="choreonoid/ext/graspPlugin/RobotInterface/Nextage/THK/HiroNX.idl"
 
 for item in ${ARRAY[@]}
 do
   P=`pwd`
   NAME=`basename $item`
   echo "Entering `dirname $item`..."
-  cd `dirname $item`
+  cd $(readlink -f `dirname $item`)
   omniidl -bpython -v $NAME
   cd $P
 done
