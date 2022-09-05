d223741d8c71e1cccdb3d3c3b4a02760ae7d2d31
kazuki0824 <kazukuro0824@gmail.com>
2022-09-06 01:22:35 +0900

    Add libusb1.0-0-dev in setup_choreonoid_master.sh

--- setup_choreonoid_master.sh
+++ setup_choreonoid_master.sh
@@ -47,7 +47,7 @@ sleep 2
 source ./choreonoid/misc/script/install-requisites-ubuntu-"$VERSION_ID".sh
 
 # Compatibility
-sudo apt install libboost-all-dev libpcl-dev liblapack-dev freeglut3-dev --no-install-recommends -y
+sudo apt install libboost-all-dev libpcl-dev liblapack-dev freeglut3-dev libusb-1.0-0-dev --no-install-recommends -y
 
 ## See: https://docs.python.org/ja/3/c-api/unicode.html
 ## バージョン 3.7 で変更: 返り値の型が char * ではなく const char * になりました。
